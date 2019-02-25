126d56f1c20f3893ecbe96ab5f4e92ce18de9b92
Sam Underwood <sam@sunderapps.com>
2019-02-24 19:25:20 -0500

    Added Audio.js

--- js/dacierno.js
+++ js/dacierno.js
@@ -3,6 +3,7 @@ var dacierno = dacierno || {
     init: function () {
         console.log('dacierno.init');
         $under.history.title = 'D\'Acierno';
+        $under.progress.color = '#f65b0b';
     }
 };
 
