b800c9f236fa53acb37c707f74bd3010a1d6513c
Sam Underwood <sam@sunderapps.com>
2019-03-03 00:45:30 -0500

    Temp/Fade

--- js/dacierno.js
+++ js/dacierno.js
@@ -1,23 +1,31 @@
 var dacierno = dacierno || {
 
-    open: function () {
-        $('.container.open:not(.hide)').off('scroll').on('scroll', dacierno.background).scroll();
+    openPage: function () {
+        $('.container.open:not(.hide)').off('scroll').on('scroll', dacierno.background.update).scroll();
     },
 
-    background: function () {
-        var $container = $('.container.open:not(.hide)');
-        if ($container) {
-            var index = Math.floor(Math.max($container.scrollTop() + ($(window).height() / 3), 0) / $(window).height());
-            var src = $($($container.children()[0]).children()[index]).find('img').attr('src');
-            $('body').css('backgroundImage', 'url(' + (src || 'https://i.imgur.com/7edvN0R.jpg') + ')');
-            console.log(index);
-            console.log(src);
+    background: {
+        default: 'https://i.imgur.com/7edvN0R.jpg',
+        update: function () {
+            var $container = $('.container.open:not(.hide)');
+            if ($container) {
+                var index = Math.floor(Math.max($container.scrollTop() + ($(window).height() / 3), 0) / $(window).height());
+                var src = $($($container.children()[0]).children()[index]).find('img').attr('src');
+                $('.body-temp').css('backgroundImage', 'url(' + (src || dacierno.background.default) + ')').addClass('show');
+                delay(function () {
+                    $('body').css('backgroundImage', 'url(' + (src || dacierno.background.default) + ')');
+                    $('.body-temp').removeClass('show');
+                }, 300);
+            }
+        },
+        
+        init: function () {
+            $('body').append($('<div></div>').addClass('body-temp'));
         }
     },
 
     events: function () {
-        $under.$erver.on('$-open-page', dacierno.open);
-        
+        $under.$erver.on('$-open-page', dacierno.openPage);
     },
 
     init: function () {
