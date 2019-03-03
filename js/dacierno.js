ba9f90f35603dccaa2d9cff4cf2943c61429af5f
Sam Underwood <sam@sunderapps.com>
2019-03-02 22:49:37 -0500

    No clue what this is.

--- js/dacierno.js
+++ js/dacierno.js
@@ -1,8 +1,22 @@
 var dacierno = dacierno || {
 
+    background: function () {
+        var $container = $('.container.open:not(.hide)');
+        if ($container) {
+            var index = $container.scrollTop() / $container.height() * $container.children().length;
+            console.log(index);
+        }
+    },
+
+    events: function () {
+        $under.$erver.on('$-open-page', dacierno.background);
+        $('.container').on('scroll', dacierno.background);
+    },
+
     init: function () {
         $under.history.title = 'D\'Acierno';
         $under.progress.color = '#f65b0b';
+        dacierno.events();
     }
 };
 
